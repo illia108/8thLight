2f8ac9a723ec329f8024b16793236963b1ab2a90
keithconroy <kconroyjr@gmail.com>
2015-11-02 18:45:21 -0800

    rename single letter and confusing variables

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,8 @@
 class Game
   def initialize
     @board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
-    @com = "X"
-    @hum = "O"
+    @computer = "X"
+    @human = "O"
   end
 
   def start_game
@@ -24,7 +24,7 @@ class Game
     until spot
       spot = gets.chomp.to_i
       if @board[spot] != "X" && @board[spot] != "O"
-        @board[spot] = @hum
+        @board[spot] = @human
       else
         spot = nil
       end
@@ -36,11 +36,11 @@ class Game
     until spot
       if @board[4] == "4"
         spot = 4
-        @board[spot] = @com
+        @board[spot] = @computer
       else
-        spot = get_best_move(@board, @com)
+        spot = get_best_move(@board, @computer)
         if @board[spot] != "X" && @board[spot] != "O"
-          @board[spot] = @com
+          @board[spot] = @computer
         else
           spot = nil
         end
@@ -51,50 +51,50 @@ class Game
   def get_best_move(board, next_player, depth = 0, best_score = {})
     available_spaces = []
     best_move = nil
-    board.each do |s|
-      if s != "X" && s != "O"
-        available_spaces << s
+    board.each do |space|
+      if space != "X" && space != "O"
+        available_spaces << space
       end
     end
-    available_spaces.each do |as|
-      board[as.to_i] = @com
+    available_spaces.each do |space|
+      board[space.to_i] = @computer
       if game_is_over(board)
-        best_move = as.to_i
-        board[as.to_i] = as
+        best_move = space.to_i
+        board[space.to_i] = space
         return best_move
       else
-        board[as.to_i] = @hum
+        board[space.to_i] = @human
         if game_is_over(board)
-          best_move = as.to_i
-          board[as.to_i] = as
+          best_move = space.to_i
+          board[space.to_i] = space
           return best_move
         else
-          board[as.to_i] = as
+          board[space.to_i] = space
         end
       end
     end
     if best_move
       return best_move
     else
-      n = rand(0..available_spaces.count)
-      return available_spaces[n].to_i
+      random = rand(0..available_spaces.count)
+      return available_spaces[random].to_i
     end
   end
 
-  def game_is_over(b)
+  def game_is_over(board)
 
-    [b[0], b[1], b[2]].uniq.length == 1 ||
-    [b[3], b[4], b[5]].uniq.length == 1 ||
-    [b[6], b[7], b[8]].uniq.length == 1 ||
-    [b[0], b[3], b[6]].uniq.length == 1 ||
-    [b[1], b[4], b[7]].uniq.length == 1 ||
-    [b[2], b[5], b[8]].uniq.length == 1 ||
-    [b[0], b[4], b[8]].uniq.length == 1 ||
-    [b[2], b[4], b[6]].uniq.length == 1
+    [board[0], board[1], board[2]].uniq.length == 1 ||
+    [board[3], board[4], board[5]].uniq.length == 1 ||
+    [board[6], board[7], board[8]].uniq.length == 1 ||
+    [board[0], board[3], board[6]].uniq.length == 1 ||
+    [board[1], board[4], board[7]].uniq.length == 1 ||
+    [board[2], board[5], board[8]].uniq.length == 1 ||
+    [board[0], board[4], board[8]].uniq.length == 1 ||
+    [board[2], board[4], board[6]].uniq.length == 1
   end
 
-  def tie(b)
-    b.all? { |s| s == "X" || s == "O" }
+  def tie(board)
+    board.all? { |space| space == "X" || space == "O" }
   end
 
 end
