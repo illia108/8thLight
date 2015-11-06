4b9456fb428e3223aa8289a2f0ebd3e66c0484d0
keithconroy <kconroyjr@gmail.com>
2015-11-06 10:49:56 -0800

    add view initialize test

--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe View do
+  let(:view){ View.new }
 
+  context "initialize" do
+    it "should be a View" do
+      expect(view).to be_a(View)
+    end
+  end
 end
