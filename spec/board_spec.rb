e410b69e2e127b4c8a6688d7cd16e5167c16fbfe
keithconroy <kconroyjr@gmail.com>
2015-11-06 10:28:16 -0800

    add board initialize test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Board do
+  let(:board){ Board.new }
+  let(:values){ [0,1,2,3,4,5,6,7,8] }
 
+  context "initialize" do
+    it "should be a Board" do
+      expect(board).to be_a(Board)
+    end
+
+    it "board should be empty" do
+      expect(board.values).to eq(values)
+    end
+
+    it "board.available_spaces should contain all spaces" do
+      expect(board.available_spaces).to eq(values)
+    end
+  end
 end
