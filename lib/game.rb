68af8de9e78bdc6e4bec5ee4c7c54667c2f08912
keithconroy <kconroyjr@gmail.com>
2015-11-08 22:08:34 -0800

    move get_computer move logic into module

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,6 @@
 require_relative 'board'
 require_relative 'player'
+require_relative 'minmax'
 
 class Game
   attr_accessor :board, :active_player, :opponent
@@ -44,8 +45,7 @@ class Game
     if @board.values[4] == 4
       return 4
     else
-      get_best_move(@board, @active_player)
-      return @choice
+      return Minmax.choice(self)
     end
   end
 
@@ -55,49 +55,6 @@ class Game
 
   def valid_move?(space)
     /^\d{1}$/ === space && @board.available_spaces.include?(space.to_i)
-  end
-
-  def score(board, depth)
-    if board.won?(@active_player)
-      return 10 - depth
-    elsif board.won?(@opponent)
-      return depth - 10
-    else
-      return 0
-    end
-  end
-
-  def get_best_move(board, player, depth = 0)
-    return score(board, depth) if board.won? || board.tie?
-    depth += 1
-    scores = {}
-
-    board.available_spaces.each do |space|
-      possible_board = board_copy(board)
-      possible_board.update_board(player.marker, space)
-      scores[space] = get_best_move(possible_board, next_player(player), depth)
-    end
-
-    if player == @active_player
-      best_score = scores.max_by{|space, score| score}
-      @choice = best_score[0]  #space
-      return best_score[1]     #score
-    else
-      best_score = scores.min_by{|space, score| score}
-      @choice = best_score[0]  #space
-      return best_score[1]     #score
-    end
-  end
-
-  def board_copy(board)
-    temp_board = Board.new
-    temp_board.values = board.values.clone
-    temp_board.available_spaces = board.available_spaces.clone
-    return temp_board
-  end
-
-  def next_player(current_player)
-    current_player == @active_player ? @opponent : @active_player
   end
 
   def won?
